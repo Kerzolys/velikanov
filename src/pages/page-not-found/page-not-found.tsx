f23ac71bceb3c28ed9091aff700bceec12224d1f
kerzolys <kerzolys@gmail.com>
2024-11-28 09:49:58 +0300

    fix: 404 page

--- src/pages/page-not-found/page-not-found.tsx
+++ src/pages/page-not-found/page-not-found.tsx
@@ -1,1 +1,13 @@
-export const PageNotFound =() => <>fuck you</>
+import { ButtonUI } from 'components/ui/button-ui/button-ui'
+import styles from './page-not-found.module.scss'
+
+export const PageNotFound = () => {
+  return (
+    <div className={styles.container}>
+      <h1>
+        404 <br /> Sorry <br />Page not found
+      </h1 >
+      <ButtonUI type='button' buttonText="Home" onClick={() => window.location.href = '/'}/>
+    </div>
+  )
+}
